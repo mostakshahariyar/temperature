80542dc60dd0ca965ed78d64982ed0f9deccb29e
mostak shahariyar <mostakshahariyar18@gmail.com>
2021-09-03 23:41:38 +0600

    complete

--- js/app.js
+++ js/app.js
@@ -1,23 +1,23 @@
-const API_KEY = `46ad7457603b9b0104e633e78cd60e16`;
+const API_KEY = `b119dee5120d18456228b9df8b214052`;
 const searchTemperature = () => {
-    const city = document.getElementById('city-name').value;
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`;
-
+    const searchField = document.getElementById('search-field').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${searchField}&appid=${API_KEY}&units=metric`
+    // console.log(url)
     fetch(url)
         .then(res => res.json())
-        .then(data => displayTemperature(data));
+        .then(data => displayTemperatureResult(data))
 }
-
-const setInnerText = (id, text) => {
+const textCity = (id, text) => {
     document.getElementById(id).innerText = text;
 }
 
-const displayTemperature = temperature => {
-    setInnerText('city', temperature.name);
-    setInnerText('temperature', temperature.main.temp);
-    setInnerText('condition', temperature.weather[0].main);
-    // set weather icon
-    const url = `http://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`;
-    const imgIcon = document.getElementById('weather-icon');
-    imgIcon.setAttribute('src', url);
+const displayTemperatureResult = temperature => {
+    textCity('city-name', temperature.name)
+    textCity('temperature', temperature.main.temp)
+    textCity('condition', temperature.weather[0].main)
+    // console.log(temperature);
+    // set weather icon 
+    const url = `https://openweathermap.org/img/wn/${temperature.weather[0].icon}@2x.png`
+    const img = document.getElementById('weather-icon');
+    img.setAttribute('src', url)
 }
